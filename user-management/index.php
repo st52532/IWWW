a4de8a146b5a4f23a64805add42711cfcc7ff8ef
st52532 <st52532@student.upce.cz>
2018-10-28 22:56:35 +0100

    DU

---
+++ user-management/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Petr
+ * Date: 28.10.2018
+ * Time: 22:16
+ */
+/**
+CREATE TABLE user
+(
+id int PRIMARY KEY AUTO_INCREMENT,
+username varchar(50),
+password varchar(50),
+email varchar(50),
+description text,
+created datetime
+)
+ */
